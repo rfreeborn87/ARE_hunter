c75bf0b3e29dc14e18bd892ba739adcf233e7a19
Robert Freeborn <robertfreeborn2@gmail.com>
2018-05-15 15:53:47 -0400

    Added regex for ARE, and tokenization for the txt

--- find_ARE.py
+++ find_ARE.py
@@ -1,14 +1,24 @@
 #This is the start of my file for ARE hunting.
 import re
-filename = 'E:/OneDrive/Research/Rockwell_Lab/Python/ARE_hunter/hIFNg_from_ECR.txt'
+from nltk.tokenize import regexp_tokenize
+filename = 'E:/OneDrive/Research/Rockwell_Lab/Python/ARE_hunter/hNQO1_from_ECR.txt'
 
 #file = open(filename, mode = 'r')
 with open(filename, 'r') as file:
     text = file.read()
     cased_text = text.upper()
-print(cased_text)
+    #Tokenize the txt file.  The first token will be the entire gene, and each subsequent token will be an evolutionarily conserved region.
+    tokenized_string = regexp_tokenize(cased_text, r'>[A-Za-z\d\s\-\:\(\)]+')
+    for token in tokenized_string:
+        print(token)
+    #ARE = TGACxxxGC
+    #regex to find this ARE: TGAC\w\w\wGC
+    ARE_regex = (r'TGAC\w\w\wGC')
+    #print(re.search(ARE_regex, cased_text))
+    #Reverse ARE = CGxxxGTCA
+    #regex to find reverse ARE: CG\w\w\wGTCA
+#print(cased_text)
 
-#ARE = TGACxxxGC
-#Reverse ARE = CGxxxGTCA
+
 
 #file.close()
