86a4f1f7e2a2ed4af90ee20bb2b4f05d7106d575
Robert Freeborn <robertfreeborn2@gmail.com>
2018-05-15 15:06:00 -0400

    Read file and uppercase all letters.

--- find_ARE.py
+++ find_ARE.py
@@ -1,1 +1,11 @@
 #This is the start of my file for ARE hunting.
+
+filename = 'E:/OneDrive/Research/Rockwell_Lab/Python/ARE_hunter/hIFNg_from_ECR.txt'
+
+#file = open(filename, mode = 'r')
+with open(filename, 'r') as file:
+    text = file.read()
+    cased_text = text.upper()
+print(cased_text)
+
+#file.close()
