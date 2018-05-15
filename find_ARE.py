8fba0d74823b63d0ad5a822e45705f3257d01d95
Robert Freeborn <robertfreeborn2@gmail.com>
2018-05-15 15:23:42 -0400

    import re and add comments containing sequences of interest

--- find_ARE.py
+++ find_ARE.py
@@ -1,5 +1,5 @@
 #This is the start of my file for ARE hunting.
-
+import re
 filename = 'E:/OneDrive/Research/Rockwell_Lab/Python/ARE_hunter/hIFNg_from_ECR.txt'
 
 #file = open(filename, mode = 'r')
@@ -7,5 +7,8 @@ with open(filename, 'r') as file:
     text = file.read()
     cased_text = text.upper()
 print(cased_text)
+
+#ARE = TGACxxxGC
+#Reverse ARE = CGxxxGTCA
 
 #file.close()
